43f4d50d368fa012345efc430171cd02a7cabf39
benchan777 <benchan777@gmail.com>
2022-02-16 20:29:24 -0800

    scrape website, serialize data into JSON, and output to file

--- scrape.go
+++ scrape.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
+	"regexp"
+	"strconv"
 
 	"github.com/gocolly/colly"
 )
+
+type ScrapedData struct {
+	MenuData string
+}
 
 // main() contains code adapted from example found in Colly's docs:
 // http://go-colly.org/docs/examples/basic/
@@ -12,12 +20,22 @@ func main() {
 	// Instantiate default collector
 	c := colly.NewCollector()
 
-	// On every a element which has href attribute call callback
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-                link := e.Attr("href")
+	c.OnHTML("body", func(e *colly.HTMLElement) {
+		// data := e.Text
+		data := ScrapedData{e.Text}
+		reg, _ := regexp.Compile("menu_data = (.*)};") // compile regexp
+		rawData := reg.FindString(data.MenuData)       // find string
 
-		// Print link
-                fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+		// ignore the first and last part of the regex data ("menu_data = " and "};")
+		length := len(rawData)
+		jsonify, _ := json.Marshal(rawData[12 : length-1])
+
+		// Since the data is already in JSON form, running json.Marshal adds an extra set of quotes causing
+		// unnecessary escaping. Remove the extra quotes so proper json can be built
+		unquoted, _ := strconv.Unquote(string(jsonify))
+
+		// Write JSON to file
+		_ = os.WriteFile("output.json", []byte(unquoted), 0644)
 	})
 
 	// Before making a request print "Visiting ..."
@@ -26,5 +44,5 @@ func main() {
 	})
 
 	// Start scraping on https://hackerspaces.org
-	c.Visit("https://hackerspaces.org/")
+	c.Visit("https://guerrero.tartine.menu/pickup/")
 }
